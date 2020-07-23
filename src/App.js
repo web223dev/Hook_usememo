d7ddfc7fb4dc71e8990627e39458054f54d53dc6
Dmitrii <ruslanus866@outlook.com>
2020-07-23 22:27:24 +0800

    use_memo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from "react"
+import { useFetch } from './useFetch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [count, setCount] = useState(0);
+  const {data} = useFetch(
+    "https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json"
   );
+
+  const computeLongestWord = useCallback(arr => {
+    if(!arr){
+      return []
+    }
+  
+    console.log("computing longest word");
+  
+    let longestWord = '';
+  
+    // console.log(data);
+  
+    JSON.parse(arr).forEach(sentence => sentence.split(" ").forEach(word => {
+      if(word.length > longestWord.length){
+        longestWord = word;
+      }
+    }))
+  
+    return longestWord;
+  }, []);
+  const longestWord = useMemo(() => computeLongestWord(data), [data, computeLongestWord]);
+
+  return (
+    <div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+      <div>{longestWord}</div>
+    </div>
+  )
 }
 
-export default App;
+export default App
